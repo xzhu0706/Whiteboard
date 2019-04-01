ff1fadbff21b4a644bc61164d5642e8bcc19b767
Xiaohong Zhu <xzhu0706@gmail.com>
2019-04-01 17:42:05 -0400

    added react cookies to store user info, created CoursePage component

---
+++ src/components/CoursePage.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import cookie from 'react-cookies';
+
+class CoursePage extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			professorID: 3,
+			userID: 0,
+			userType: 3,
+			// maybe more stuff...
+		}
+
+	}
+
+	componentDidMount() {
+		let userID = cookie.load('userID');
+		let userType = cookie.load('userType');
+		this.setState({
+			userID: userID,
+			userType: userType,
+		});
+		console.log(userType)
+
+		// load course info from API and save to states
+
+
+		if (userType == 1) {   // userType = 1 = professor
+			if (userID != this.state.professorID) {
+				window.location = '/home/' + this.state.userID;
+			}
+		}
+	}
+
+
+
+	render() {
+		return (
+			<div>
+				<h1> Course Page </h1>
+				<h2> id: {this.state.userID ? this.state.userID : 'nothing'}</h2>
+			</div>
+
+		);
+	}
+}
+
+export default CoursePage;
